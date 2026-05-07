dd142af37f32acd3c99d6a1b9877118e6e08749f
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-07 07:34:48 +0000

    Repository for Review is created

--- src/main/java/com/example/nxttrendz1/repository/ReviewRepository.java
+++ src/main/java/com/example/nxttrendz1/repository/ReviewRepository.java
@@ -7,3 +7,21 @@
  */
 
 // Write your code here
+package com.example.nxttrendz1.repository;
+
+import com.example.nxttrendz1.model.*;
+import java.util.*;
+
+public interface ReviewRepository {
+    List<Review> getAllReview();
+
+    Review addNewReview(Review review);
+
+    Review getReviewById(int reviewId);
+
+    Review updateReview(int reviewId, Review review);
+
+    void deleteReview(int reviewId);
+
+    Product getProductByReviewId(int reviewId);
+}
