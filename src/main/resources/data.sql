a2e390deb42b008cd000ff4127e9add50d2bbd77
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-07 06:21:06 +0000

    add first commit by adding project structure(files)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO product(productName, price) 
+VALUES
+('Smartphone', 599.99),
+('Laptop', 1299.99),
+('Gaming Console', 399.99);
+
+
+INSERT INTO review(reviewContent, rating, productId) 
+VALUES
+('Great battery life!', 5, 1),
+('Lags sometimes.', 3, 1),
+('Perfect for my daily tasks!', 4, 2),
+('Bit pricey, but worth it.', 4, 2),
+('Awesome gaming experience!', 5, 3),
+('Needs more exclusive games.', 4, 3);
